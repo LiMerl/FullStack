6f8dc581e5d720c1ccfdaa797ff721a91d28dc15
LiMerl <73393994+LiMerl@users.noreply.github.com>
2022-12-01 20:34:04 +0100

    Add files via upload

---
+++ app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { ComponentRef, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+
+import { VaccinationCenterListComponent } from './vaccination-center-list/vaccination-center-list.component';
+import { VaccinationCenterComponent } from './vaccination-center/vaccination-center.component';
+import { WaitingComponent } from './waiting/waiting.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'connection', pathMatch: 'full'},
+  {path: "connection", component: LoginComponent},
+  {path: "centers", component: VaccinationCenterListComponent},
+  {path: "centers/detail/:id", component: VaccinationCenterComponent},
+  {path: 'waiting/:temps',component: WaitingComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
